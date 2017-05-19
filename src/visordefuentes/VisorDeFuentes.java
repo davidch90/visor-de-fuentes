431077ea01072e0af1b907a72b81e72174a718a7
davidch90 <15470182@itcampeche.edu.mx>
2017-05-19 17:26:53 -0500

    proyecto enviado al repositorio local

---
+++ src/visordefuentes/VisorDeFuentes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visordefuentes;
+
+import gui.MainForm;
+
+/**
+ *
+ * @author Davidch 
+ * 
+ */
+public class VisorDeFuentes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    MainForm ventana = new MainForm();
+          ventana.setVisible(true);
+    
+    }
+    
+}
